ad9cf7a99d4421b607c7590cef071d083d10fffa
Mehedi Hasan <mhasan172036@mscse.uiu.ac.bd>
2021-07-10 03:58:21 +0600

    3rd commit

--- src/test.java
+++ src/test.java
@@ -1,6 +1,17 @@
 public class test{
     public static void main(String[] args) {
+
+        test obj = new test();
+        obj.add();
+
         String mes = "Hello, world!"; 
         System.out.println(mes);
+    }
+    public void add(){
+        int a = 10;
+        int b = 5;
+        int sub = a+b;
+        System.out.println(sub);
+
     }
 }
